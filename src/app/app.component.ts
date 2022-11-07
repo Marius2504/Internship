92c34993c70b8aedc5a8e2696864ed086d69719a
Marius Cristian Dumitrescu <Marius.Cristian.Dumitrescu@ibm.com>
2022-11-07 16:39:34 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title ='da'
+  users:any
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+      this.http.get('https://localhost:44311/api/users').subscribe({
+        next:Response => {this.users = Response, console.log(Response)}, 
+        error:error =>console.log(error)
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -10,9 +12,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements OnInit {
+  title ='da'
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+      this.http.get('https://localhost:5000/api/users').subscribe(Response => {
+        this.title = Response.toString()
+      },error =>{
+        console.log(error)
+      })
+  }
+ }
